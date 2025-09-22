07ba6b0dc2b107f2a45b6d3c4f9c71e42795b435
AlejandroNavas18 <alejandro.navas.rodriguez.alu@iesjulioverne.es>
2025-09-22 12:34:29 +0200

    Funcion de suma agregada

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,13 @@
-
+public class Calculadora{
+		public static void main(String [] args){
+			
+			System.out.println("Ejemplo de suma: "+ suma(2,10));
+		}
+			
+		//Implementamos la suma
+		public double suma (int a, int b){
+		
+		return a+b;
+		
+		}
+	}
