928b2fc98ad31a6c503c1de714d494b9126743c2
pablogarciaordonezalu <pablo.garcia.ordonez.alu@iesjulioverne.es>
2025-09-22 12:32:29 +0200

    division agregada

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,12 @@
+public class Calculadora{
+    
+    public static void main(String[] args){
+        System.out.println("Ejemplo de division: "+division(10,2));
 
+    }
+    public int division(int dividendo, int divisor){
+       double resultado = dividendo/divisor;
+
+       return resultado; 
+    }
+}
